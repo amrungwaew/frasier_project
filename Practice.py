54876ae244f1c90d8e9653ba89ff305a77e4bd9b
Anna Jeffries <jeffri66@msu.edu>
2022-10-24 15:46:12 -0400

    Update Practice.py

--- Practice.py
+++ Practice.py
@@ -149,9 +149,10 @@ with st.container():
     with col11a:
         ch_season_plot
 
+    achoice = st.checkbox('I would like to compare ' + ch_select + ' with another main character')
 
     with col22a:
-        achoice = st.checkbox('I would like to compare ' + ch_select + ' with another main character')
+
 
         if achoice:
             
