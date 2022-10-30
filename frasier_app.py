3c2a43f1162e235661875850b4373f00aaa5f1ab
Anna Jeffries <70072027+amrungwaew@users.noreply.github.com>
2022-10-30 15:05:21 -0400

    Update frasier_app.py

--- frasier_app.py
+++ frasier_app.py
@@ -243,7 +243,7 @@ with st.container():
         y=alt.Y('total_words',axis=alt.Axis(title='Total number of words')),
         color=alt.Color('characterName',scale=alt.Scale(scheme='rainbow'),
         legend=alt.Legend(title='Characters', orient='bottom')),
-        tooltip=['title','total_words','actorName','gender','imdbRatings','viewershipInMillions'])
+        tooltip=['title','total_words','actorName','gender','imdbRatings','viewershipInMillions']).interactive()
 
     # areas = alt.Chart(seasons_rect).mark_rect(opacity=0.2).encode(
     #         x='start', x2='stop',
