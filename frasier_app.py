8581d9e338972d7073647970caf60f4e0a47a8c5
Anna Jeffries <70072027+amrungwaew@users.noreply.github.com>
2022-10-30 14:19:06 -0400

    Update frasier_app.py

--- frasier_app.py
+++ frasier_app.py
@@ -81,11 +81,13 @@ viewing_avg = []
 for i in range(1,12):
     viewing_avg.append(mean(df_frasier_totalwords['viewershipInMillions'].where(df_frasier_totalwords['season'] == i).dropna()))
 
-df_frasier_totalwords['rating_avg'] = rating_avg
-df_frasier_totalwords['viewing_avg'] = viewing_avg
 
-df_frasier_characterwords['rating_avg'] = rating_avg
-df_frasier_characterwords['viewing_avg'] = viewing_avg
+
+df_frasier_totalwords['rating_avg'] = pd.DataFrame(rating_avg)
+df_frasier_totalwords['viewing_avg'] = pd.DataFrame(viewing_avg)
+
+df_frasier_characterwords['rating_avg'] = pd.DataFrame(rating_avg)
+df_frasier_characterwords['viewing_avg'] = pd.DataFrame(viewing_avg)
 
 # plotting the season chart with total words by season
 season_plot = alt.Chart(df_frasier_totalwords,padding={'left': 0, 'top': 25, 'right': 0, 'bottom': 5}).mark_bar(size=30).encode(
