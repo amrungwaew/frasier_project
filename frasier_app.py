8f81c8297601c1493b964d5a1cc4a0a350a9d2a3
Anna Jeffries <70072027+amrungwaew@users.noreply.github.com>
2022-11-03 13:27:20 -0400

    Update frasier_app.py

--- frasier_app.py
+++ frasier_app.py
@@ -251,7 +251,8 @@ with st.container():
 
     areas = alt.Chart(seasons_rect.reset_index()).mark_rect(opacity=0.1).encode(
             x='start', x2='stop',
-            color='index:N')
+            color=alt.Color('index:N',scale=alt.Scale(scheme='rainbow'),
+            legend=alt.Legend(title='Seasons', orient='bottom')))
 
     (areas + ch_show_plot)
 
